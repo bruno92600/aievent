cd4d55a28036fc81c951008444ee70d27424d1f9
bruno <pascoal.goncalves.bruno@gmail.com>
2025-11-30 20:44:38 +0100

    first

---
+++ components/header.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b"> 
+        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+          {/* Logo */}
+          <Link href={"/"} className="flex items-center">
+          <Image
+            src="/spott.png"
+            alt="logo"
+            width={500}
+            height={500}
+            className="w-full h-11"
+            priority
+          />
+          </Link>
+
+          {/* Navigation Links search & location */}
+
+          {/* right side actions */}
+        </div>
+
+        {/* Mobile Menu */}
+      </nav>
+
+      {/* modals */}
+    </>
+  );
+};
+
+export default Header;
